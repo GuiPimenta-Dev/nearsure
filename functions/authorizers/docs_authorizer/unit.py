4facc73c0bc19bd5c704d6c24ffaa581d4623202
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-17 22:09:13 -0300

    Initial commit

---
+++ functions/authorizers/docs_authorizer/unit.py
@@ -0,0 +1,45 @@
+from .main import lambda_handler
+
+def test_authorizer_should_pass_with_correct_secret():
+
+    event = {
+        "headers": {
+            "secret": "A8jeCXTUequAaN7f1RUlVS3IbPmhw8ky5Yr2NlOToLqeKUlHx1kc"
+        }
+    }
+    response = lambda_handler(event, None)
+
+    assert response == {
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": "allow",
+                    "Resource": "*"
+                }
+            ],
+        },
+    }
+
+def test_authorizer_should_fail_with_invalid_secret():
+
+    event = {
+        "headers": {
+            "secret": "INVALID-SECRET"
+        }
+    }
+    response = lambda_handler(event, None)
+
+    assert response == {
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": "deny",
+                    "Resource": "*"
+                }
+            ],
+        },
+    }
